c4ea6065937392963449c35ce332a8c1b36a1acf
huoyiming <dghuoyiming@163.com>
2021-11-27 18:36:25 +0800

    优化

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pikepdf import Pdf
 #说明
 print("请在脚本同一目录下编辑list.txt文件\n格式：班别(文件名)+姓名+身份证号，一行一组")
 print("信息不匹配将无法输出，并在同一目录下生成error.txt名单\n仅对广东省注册志愿者有效\n\n")
-print("正在执行脚本...\n\n")
 #列表处理
 list=[]                                                 #初始化二维列表
 with open("list.txt", "r", encoding="utf-8") as f:      
@@ -22,25 +21,37 @@ newfile=open("error_list.txt", "w+",encoding="utf-8")
 newfile.close()
 #刷新url与下载
 for i in list:                                          #循环更新url
-    url="https://www.gdzyz.cn/srvpg/downloadCertificateFromBD?idcardCode="+i[2]+"&userName="+i[1]
+    print('正在下载',i[0]+"-"+i[1])
+    url= "https://www.gdzyz.cn/srvpg/downloadCertificateFromBD?idcardCode="+i[2]+"&userName="+i[1]
     r = requests.get(url) 
     if len(r.text) == 0:                                #判断响应文本是否为空
+        print('##下载出错##\n')
         errorlist = "\t".join(i)+"\n"                 #将一维列表写入字符串
         with open("error_list.txt", "a+",encoding="utf-8") as error:    #储存错误的名单
             error.write(errorlist)
+        list.remove(i)#将发生错误的一维列表从二维列表中删除
     else:
+        print('##下载成功##\n')
         with open(i[0]+"-"+i[1]+".pdf", "wb") as pdf:    #储存
             pdf.write(r.content)
-        origin = Pdf.open(i[0]+"-"+i[1]+".pdf")
-        dst = Pdf.new()
-        dst.pages.append(origin.pages[0])
+
+print('是否删除PDF第二页（目前是空白页面）')
+delete=''
+delete=input('删除请输入yes并回车，直接按回车跳过此步骤：')
+print('\n')
+if delete == "yes":
+    for i in list:
+        print('正在处理'+i[0]+"-"+i[1]+'\n')
+        origin = Pdf.open(i[0]+"-"+i[1]+".pdf")#读取pdf
+        dst = Pdf.new()#新建pdf
+        dst.pages.append(origin.pages[0])#将原始pdf的第一页添加到新建pdf中
         save_name=i[0]+"-"+i[1]+".pdf"
-        dst.save(save_name)
+        dst.save(save_name)#保存
 #数error.txt的行数，统计错误
 count = -1
 for count, line in enumerate(open("error_list.txt", "r",encoding="utf-8")):
     pass
 count += 1
 
-print("输出完成,共%d个错误,请查收生成的文件"%count)
+print("##输出完成,共%d个错误,请查收生成的文件##\n"%count)
 input("按回车退出")
